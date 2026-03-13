425a4eb294e44f73b0d63cc2702b178df788b6cb
pemply <pemply56@gmail.com>
2026-03-13 13:23:23 +0200

    arena

---
+++ Assets/Plugins/AllIn1VfxToolkit/Demo & Assets/Demo/Scripts/AllIn1VfxFadeLight.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace AllIn1VfxToolkit.Demo.Scripts
+{
+    [RequireComponent(typeof(Light))]
+    public class AllIn1VfxFadeLight : MonoBehaviour
+    {
+        [SerializeField] private float fadeDuration = 0.1f;
+
+        private Light _targetLight;
+        private float _animationRatioRemaining;
+        private float _initialLightIntensity;
+
+        private void Awake()
+        {
+            _targetLight = GetComponent<Light>();
+            _initialLightIntensity = _targetLight.intensity;
+        }
+
+        private void OnEnable()
+        {
+            if (_targetLight == null)
+                _targetLight = GetComponent<Light>();
+
+            _animationRatioRemaining = 1f;
+            _targetLight.intensity = _initialLightIntensity;
+        }
+
+        private void Update()
+        {
+            if (_targetLight == null)
+                return;
+
+            _targetLight.intensity = Mathf.Lerp(0f, _initialLightIntensity, _animationRatioRemaining);
+            _animationRatioRemaining -= Time.deltaTime / fadeDuration;
+
+            if (_animationRatioRemaining < 0f)
+                _animationRatioRemaining = 0f;
+        }
+    }
+}
